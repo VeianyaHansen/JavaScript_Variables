2409094a5a24621540601a5ea3b9a84c55bc2ec5
Veianya Hansen <melaya0105@gmail.com>
2021-04-12 16:06:01 -0700

    Completed

--- app.js
+++ app.js
@@ -4,7 +4,21 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Veianya";
+let lastName = "Hansen";
+var myAge = 37;
 
 // Exercise 2
+let fullName = firstName + " " + lastName;
+let templateFullName = `${firstName} ${lastName}`;
+console.log("fullName: ", fullName);
+console.log("templateFullName: ", templateFullName);
 
 // Exercise 3
+let myStory;
+let city = "Phoenix";
+let pastime = "code, listen to music, read, volunteer, and go walking";
+
+myStory = `My name is ${fullName}. I live in ${city} and I like to ${pastime}.`;
+
+console.log(myStory);
